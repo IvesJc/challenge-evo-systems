a17ed20fc1008d8b34b3be9a2225b091d1011827
Ives <ivesjundi@gmail.com>
2024-08-19 21:11:35 -0300

    feat :sparkles: search screen on the way

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:challenge_evo_systems/screens/all_movies/all_movies_screen.dart';
 import 'package:challenge_evo_systems/screens/home/home_screen.dart';
+import 'package:challenge_evo_systems/screens/search/search_movies.dart';
 import 'package:challenge_evo_systems/screens/test.dart';
 import 'package:flutter/material.dart';
 
--- lib/screens/search/search_movies.dart
+++ lib/screens/search/search_movies.dart
@@ -1,5 +1,8 @@
 import 'dart:convert';
 
+import 'package:challenge_evo_systems/commom/constants/app_colors.dart';
+import 'package:challenge_evo_systems/commom/constants/app_text_styles.dart';
+import 'package:challenge_evo_systems/commom/widgets/backgroud.dart';
 import 'package:challenge_evo_systems/model/movie/movie.dart';
 import 'package:flutter/material.dart';
 
@@ -15,14 +18,22 @@ class SearchMovies extends StatefulWidget {
 
 class _SearchMoviesState extends State<SearchMovies> {
 
-  Future<List<Movie>> getSearchMovies() async {
-        const url =
-        'https://api.themoviedb.org/3/search/movie?include_adult=false&language=pt-BR&page=1api_key=69e934a01daf26db696f2e0461964a4a';
+  final TextEditingController _controller = TextEditingController();
+  int _page = 1;
+  String _query = '';
+  List<dynamic> _movies = [];
+
+  void getSearchMovies() async {
+        final url =
+        'https://api.themoviedb.org/3/search/movie?query=$_query&include_adult=false&pt-BR&page=$_page&api_key=69e934a01daf26db696f2e0461964a4a';
     final response = await http.get(Uri.parse(url));
 
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
-      return data.map((movie) => Movie.fromJson(movie)).toList();
+      // data.map((movie) => Movie.fromJson(movie)).toList();
+      setState(() {
+        _movies = data['results'];
+      });
     } else {
       // Handle error
       throw Exception('Erro ao carregar filmes');
@@ -37,6 +48,70 @@ class _SearchMoviesState extends State<SearchMovies> {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: AppColors.backgroundColorDark,
+          elevation: 0,
+          title: Image(
+              image: AssetImage('assets/images/ivesflix.png'),
+              fit: BoxFit.cover,
+              height: 40,
+              filterQuality: FilterQuality.high),
+        ),
+              body: BackgroudContainer(
+        child: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('All Movies', style: AppTextStyles.mediumTextWhite),
+                const SizedBox(height: 30),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      TextField(
+                        controller: _controller,
+                        decoration: InputDecoration(
+                          labelText: 'Pesquisar',
+                          suffixIcon: IconButton(
+                            icon: Icon(Icons.search),
+                            onPressed: () {
+                              setState(() {
+                                _query = _controller.text;
+                                getSearchMovies();
+                              });
+                            },
+                          ),
+                        ),
+                        keyboardType: TextInputType.number,
+                      ),
+                      TextField(
+                        controller: _controller,
+                        decoration: InputDecoration(
+                          labelText: 'Número da página',
+                          suffixIcon: IconButton(
+                            icon: Icon(Icons.search),
+                            onPressed: () {
+                              setState(() {
+                                _page = int.tryParse(_controller.text) ?? 1;
+                                getSearchMovies();
+                              });
+                            },
+                          ),
+                        ),
+                        keyboardType: TextInputType.number,
+                      ),
+                    ]
+                  )
+                )
+              ]
+            )
+          )
+        )
+              )
+    );
   }
 }
